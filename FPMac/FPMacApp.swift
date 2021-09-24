f7f6805687e1de79f16a0815319c2dc5143e0b38
Emre Yasa <emreyasa86@gmail.com>
2021-09-24 06:43:39 -0700

    Initial Commit

---
+++ FPMac/FPMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  FPMacApp.swift
+//  FPMac
+//
+//  Created by Emre Yasa on 9/24/21.
+//
+
+import SwiftUI
+
+@main
+struct FPMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FPMacUITests/FPMacUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FPMacUITestsLaunchTests.swift
+//  FPMacUITests
+//
+//  Created by Emre Yasa on 9/24/21.
+//
+
+import XCTest
+
+class FPMacUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
